1ef59c57052143c81c5f13c0b44003911db99d64
Konstantin Vetlugin <k.a.vetlugin@yandex.ru>
2017-10-02 00:16:10 +0400

    JUnit

--- src/test/java/ru/niias/fsm/SpringImpTest.java
+++ src/test/java/ru/niias/fsm/SpringImpTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertThat;
 public class SpringImpTest
     extends TestCase
 {
+    //Создание и тестирование конечного автомата
     @Test
     public void testFDM() throws Exception{
         SpringImp si = new SpringImp();
@@ -19,9 +20,19 @@ public class SpringImpTest
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.INIT));
         stateMachine.sendEvent(Events.REQUEST);
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.REQUEST));
-
-
-
-
+        stateMachine.sendEvent(Events.UPDATE);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.FORM_UPDATED));
+        stateMachine.sendEvent(Events.LOAD_ERROR);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.LOAD_ERROR));
+        stateMachine.sendEvent(Events.LOAD);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.LOADED));
+        stateMachine.sendEvent(Events.TIMER);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.LOADED));
+        stateMachine.sendEvent(Events.CONFIRM_ERROR);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.CONFIRM_ERROR));
+        stateMachine.sendEvent(Events.FINISH);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.FINISHED));
+        stateMachine.sendEvent(Events.REQUEST);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.REQUEST));
     }
 }
