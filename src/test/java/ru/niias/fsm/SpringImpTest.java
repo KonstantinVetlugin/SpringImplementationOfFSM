1e4a3f7d28e726a9d42584cfdc2fa64934dcd50e
Konstantin Vetlugin <k.a.vetlugin@yandex.ru>
2017-10-02 12:50:58 +0300

    File SpringImpTest.java was changed

--- src/test/java/ru/niias/fsm/SpringImpTest.java
+++ src/test/java/ru/niias/fsm/SpringImpTest.java
@@ -20,19 +20,45 @@ public class SpringImpTest
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.INIT));
         stateMachine.sendEvent(Events.REQUEST);
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.REQUEST));
+        stateMachine.sendEvent(Events.TIMER);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.REQUEST));
+        stateMachine.sendEvent(Events.FINISH);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.INIT));
         stateMachine.sendEvent(Events.UPDATE);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.FORM_UPDATED));
+        stateMachine.sendEvent(Events.REQUEST);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.REQUEST));
+        stateMachine.sendEvent(Events.UPDATE);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.FORM_UPDATED));
+        stateMachine.sendEvent(Events.TIMER);
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.FORM_UPDATED));
         stateMachine.sendEvent(Events.LOAD_ERROR);
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.LOAD_ERROR));
+        stateMachine.sendEvent(Events.LOAD);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.LOADED));
+        stateMachine.sendEvent(Events.UPDATE);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.FORM_UPDATED));
         stateMachine.sendEvent(Events.LOAD);
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.LOADED));
         stateMachine.sendEvent(Events.TIMER);
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.LOADED));
         stateMachine.sendEvent(Events.CONFIRM_ERROR);
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.CONFIRM_ERROR));
+        stateMachine.sendEvent(Events.UPDATE);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.FORM_UPDATED));
+        stateMachine.sendEvent(Events.LOAD_AND_CONFIRM);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.CONFIRMED));
+        stateMachine.sendEvent(Events.UPDATE);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.FORM_UPDATED));
+        stateMachine.sendEvent(Events.LOAD);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.LOADED));
+        stateMachine.sendEvent(Events.CONFIRM);
+        assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.CONFIRMED));
         stateMachine.sendEvent(Events.FINISH);
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.FINISHED));
         stateMachine.sendEvent(Events.REQUEST);
         assertThat(stateMachine.getState().getIds(), containsInAnyOrder(States.REQUEST));
+
+        System.out.println("Current state is " + stateMachine.getState().getId());
     }
 }
